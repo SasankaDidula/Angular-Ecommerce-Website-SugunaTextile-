72b2980c9a518580ab53ada22933f88155762335
SasankaDidula <61312369+SasankaDidula@users.noreply.github.com>
2020-03-19 23:16:13 +0530

    Cart badge

--- Website/src/app/bs-navbar/bs-navbar.component.ts
+++ Website/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,19 +1,33 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Appuser } from '../models/app-user';
 import { Router } from '@angular/router';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit{
 
   appUser: Appuser;
+  shoppingCartItemCount: number;
 
-  constructor(private auth: AuthService, private router: Router) {
-    auth.appUsers$.subscribe(appUser => this.appUser = appUser);
+  constructor(private auth: AuthService, private router: Router, private shoppingcartService: ShoppingCartService) {
+    
+   }
+
+   async ngOnInit(){
+    this.auth.appUsers$.subscribe(appUser => this.appUser = appUser);
+
+    let cart$ = await this.shoppingcartService.getCart();
+    cart$.valueChanges().subscribe(cart =>{
+      this.shoppingCartItemCount = 0;
+      for ( let productId in cart.items)
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+    });
    }
 
    logout() {
