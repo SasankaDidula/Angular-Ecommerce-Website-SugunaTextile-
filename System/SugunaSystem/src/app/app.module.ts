e03cf40e50e739504dd30e2b043b578a2df3d176
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-03-25 13:48:54 +0530

    commit 1
    commit 1

--- System/SugunaSystem/src/app/app.module.ts
+++ System/SugunaSystem/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuppliersComponent } from './suppliers/suppliers.component';
+import { SupplierComponent } from './suppliers/supplier/supplier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuppliersComponent,
+    SupplierComponent
   ],
   imports: [
     BrowserModule,
