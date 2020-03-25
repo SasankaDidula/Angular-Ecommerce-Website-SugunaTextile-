b48f97648eed8dfdd58dbd979de871083fecf44c
SasankaDidula <61312369+SasankaDidula@users.noreply.github.com>
2020-03-25 14:37:00 +0530

    System update & Suppliers

---
+++ System/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SuppliersComponent } from './suppliers/suppliers.component';
+import { SupplierComponent } from './suppliers/supplier/supplier.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuppliersComponent,
+    SupplierComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
