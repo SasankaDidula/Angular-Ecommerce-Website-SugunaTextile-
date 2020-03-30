3f5a44e42a44e7bada2f5efca87281e17763e534
Priyanka-Kugapriya <50516526+Priyanka-Kugapriya@users.noreply.github.com>
2020-03-30 11:28:50 +0530

    Update app.module.ts

--- System/src/app/app.module.ts
+++ System/src/app/app.module.ts
@@ -29,6 +29,11 @@ import { SupplierListComponent } from './suppliers/supplier-list/supplier-list.c
 import {MatDialogRef} from '@angular/material/dialog';
 import { OrderTypesService } from "./shared/order-types.service";
 import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductStockComponent } from './products/product-stock/product-stock.component';
+import { ProductStockListComponent } from './products/product-stock-list/product-stock-list.component';
 
 
 @NgModule({
@@ -40,7 +45,12 @@ import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dial
     EmployeeListComponent,
     EmployeesComponent,
     EmployeeComponent,
-    MatConfirmDialogComponent
+    MatConfirmDialogComponent,
+    ProductComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductStockComponent,
+    ProductStockListComponent
     
   ],
   imports: [
