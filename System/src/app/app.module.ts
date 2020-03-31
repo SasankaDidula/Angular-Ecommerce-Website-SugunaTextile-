d5fe8267c522085dd95a57950b0ee4abc6f277c8
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-03-31 23:05:07 +0530

    commit

--- System/src/app/app.module.ts
+++ System/src/app/app.module.ts
@@ -13,25 +13,20 @@ import { SupplierComponent } from './suppliers/supplier/supplier.component';
 import { SuppliersService } from './shared/suppliers.service';
 import { environment } from "../environments/environment";
 import { OrderTypesService } from "./shared/order-types.service";
-
 import { SupplierListComponent } from './suppliers/supplier-list/supplier-list.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
-
-
 import { DatePipe } from '@angular/common';
 import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 import { DepartmentService } from './shared/department.service';
 import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.component';
-
 import { SuppliesComponent } from './suppliers/supplies/supplies.component';
 import { SuppliesService } from "./shared/supplies.service";
-
+import { MatTabsModule } from '@angular/material/tabs';
 import { SupplyComponent } from './suppliers/supplies/supply/supply.component';
 import { MatSliderModule } from '@angular/material/slider';
 import {  MatDialogModule } from '@angular/material/dialog';
 import {  MatProgressBarModule } from '@angular/material/progress-bar';
-
 import {MatExpansionModule} from '@angular/material/expansion';
 
 
@@ -69,7 +64,8 @@ import {MatExpansionModule} from '@angular/material/expansion';
       AngularFireDatabaseModule,
       AngularFireModule.initializeApp(environment.firebaseConfig),
       MatExpansionModule,
-      AngularFireModule.initializeApp(environment.firebaseConfig)
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+      MatTabsModule
    ],
    providers: [
       SuppliersService,
