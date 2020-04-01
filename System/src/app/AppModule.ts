f9ea7f4cacbd562c110fc360c2880f3ffc20fe41
Jerry.P <jeremiah.p@blackvt.com>
2020-04-02 01:03:51 +0530

    commit-mayu-promopage
    Signed-off-by: Jerry.P <jeremiah.p@blackvt.com>

---
+++ System/src/app/AppModule.ts
@@ -0,0 +1,17 @@
+// import { SuppliesComponent } from './suppliers/supplies/supplies.component';
+// import { SuppliesService } from "./shared/supplies.service";
+// import { SupplyComponent } from './suppliers/supplies/supply/supply.component';
+// import { SuppliesComponent } from './suppliers/supplies/supplies.component';
+// import { SuppliesService } from "./shared/supplies.service";
+// import { SupplyComponent } from './suppliers/supplies/supply/supply.component';
+
+// import { PromoService } from 'src/app/shared/promo.service';
+// import { NotifcationService } from 'src/app/shared/notifcation.service';
+// import { MatDialogRef } from "@angular/material";
+
+
+
+
+
+
+
