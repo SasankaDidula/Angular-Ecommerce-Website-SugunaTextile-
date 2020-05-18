925fde9abecbebba7a932f3f90019e2bd955eee5
Lameshan <>
2020-05-18 09:48:20 +0530

    fix forgotpassword

--- Website/src/app/app-routing.module.ts
+++ Website/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './core/components/sign-in/sign-in.component';
+import { SignUpComponent } from './core/components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './core/components/forgot-password/forgot-password.component';
 @NgModule({
-  declarations: [],
+  declarations: [
+
+    
+  ],
   imports: [
     CommonModule
   ]
